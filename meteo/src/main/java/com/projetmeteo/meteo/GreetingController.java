b8f663a19d48872deda27fe8faf59a7ce08ebbf4
NoahBREIZH <noah.davenel@orange.fr>
2023-12-12 13:52:37 +0100

    ok

--- meteo/src/main/java/com/projetmeteo/meteo/GreetingController.java
+++ meteo/src/main/java/com/projetmeteo/meteo/GreetingController.java
@@ -1,5 +1,6 @@
 package com.projetmeteo.meteo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
+	@Autowired
+	WeatherDataRepository repo;
 
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="à tous") String name, Model model) {
+		//repo.
 		model.addAttribute("name", name);
 		return "greeting";
 	}
--- meteo/src/main/java/com/projetmeteo/meteo/WeatherDataRepository.java
+++ meteo/src/main/java/com/projetmeteo/meteo/WeatherDataRepository.java
@@ -1,7 +1,7 @@
 package com.projetmeteo.meteo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WeatherDataRepository extends CrudRepository<WeatherData, Long> {
+public interface WeatherDataRepository extends JpaRepository<WeatherData, Long> {
 
 }
