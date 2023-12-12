9bc4392e3d8a5e6386136be908225edda68ffcc3
NoahBREIZH <noah.davenel@orange.fr>
2023-12-12 13:26:43 +0100

    Oui

--- meteo/src/main/java/com/projetmeteo/meteo/WeatherDataRepository.java
+++ meteo/src/main/java/com/projetmeteo/meteo/WeatherDataRepository.java
@@ -1,9 +1,7 @@
 package com.projetmeteo.meteo;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface WeatherDataRepository extends CrudRepository<WeatherData, Long> {
-  
+
 }
--- meteo/src/test/java/com/projetmeteo/meteo/MeteoApplicationTests.java
+++ meteo/src/test/java/com/projetmeteo/meteo/MeteoApplicationTests.java
@@ -1,7 +1,5 @@
 package com.projetmeteo.meteo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 class MeteoApplicationTests {
 
