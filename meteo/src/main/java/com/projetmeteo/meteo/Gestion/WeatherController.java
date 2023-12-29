a15799edb3c513462488300f0217a2d88c761a9c
benzar80 <100301600+benzar80@users.noreply.github.com>
2023-12-29 22:40:38 +0100

    push

--- meteo/src/main/java/com/projetmeteo/meteo/Gestion/WeatherController.java
+++ meteo/src/main/java/com/projetmeteo/meteo/Gestion/WeatherController.java
@@ -1,9 +1,15 @@
 package com.projetmeteo.meteo.Gestion;
 
 import java.time.LocalTime;
+import java.util.List;
 
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.projetmeteo.meteo.ClasseMeteo.WeatherDataCity;
+import com.projetmeteo.meteo.ClasseMeteo.WeatherDataDay;
+import com.projetmeteo.meteo.ClasseMeteo.WeatherDataHour;
+import com.projetmeteo.meteo.ClasseMeteoJSON.WeatherDay;
+import com.projetmeteo.meteo.ClasseMeteoJSON.WeatherHour;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -28,42 +34,64 @@ public class WeatherController {
 
     @PostConstruct
     public void init() {
-        Model model = new ExtendedModelMap();
-        submitWeatherData("Paris", model);
-        submitWeatherData("Lyon", model);
-        submitWeatherData("Lille", model);
-        submitWeatherData("Marseille", model);
-        submitWeatherData("Rennes", model);
-        submitWeatherData("Bordeaux", model);
+        // Model model = new ExtendedModelMap();
+        // submitWeatherData("Paris", model);
+        // submitWeatherData("Lyon", model);
+        // submitWeatherData("Lille", model);
+        // submitWeatherData("Marseille", model);
+        // submitWeatherData("Rennes", model);
+        // submitWeatherData("Bordeaux", model);
     }
 
     @Scheduled(cron = "0 0 5 * * *", zone = "Europe/Paris")
     public void updateWeatherData() {
-        repo.deleteAll();
-        Model model = new ExtendedModelMap();
-        submitWeatherData("Paris", model);
-        submitWeatherData("Lyon", model);
-        submitWeatherData("Lille", model);
-        submitWeatherData("Marseille", model);
-        submitWeatherData("Rennes", model);
-        submitWeatherData("Bordeaux", model);
+        // repo.deleteAll();
+        // Model model = new ExtendedModelMap();
+        // submitWeatherData("Paris", model);
+        // submitWeatherData("Lyon", model);
+        // submitWeatherData("Lille", model);
+        // submitWeatherData("Marseille", model);
+        // submitWeatherData("Rennes", model);
+        // submitWeatherData("Bordeaux", model);
     }
 
-	@PostMapping("/test")
+    @PostMapping("/test")
     public String submitWeatherData(@RequestParam String city, Model model) {
         city = city.toLowerCase();
         try {
-            Iterable<WeatherDataCity> weatherDataList = repo.findAllByaddress(city);
+            List<WeatherDataCity> weatherDataList = repo.findAllByaddress(city);
             if (Iterables.isEmpty(weatherDataList)) {
                 weatherService.saveDownloadDay(city);
             }  
             weatherDataList = repo.findAllByaddress(city);
             model.addAttribute("weatherDataLists", weatherDataList);
+    // Récupérer la première journée de météo
+    WeatherDataDay firstWeatherDay = weatherDataList.get(0).getWeatherDay().get(0);
+
+    // Récupérer les heures de la première journée
+    List<WeatherDataHour> weatherHours = firstWeatherDay.getWeatherHour();
+
+    // Diviser les heures en groupes de 3
+    List<List<WeatherDataHour>> groupedWeatherHours = Lists.partition(weatherHours, 3);
+            System.out.println(groupedWeatherHours);
+    // Ajouter la liste groupée au modèle
+    model.addAttribute("groupedWeatherHours", groupedWeatherHours);
+
+    // Obtenez l'heure actuelle
+LocalTime currentTime = LocalTime.now();
+
+// Calculez l'index de la slide correspondant à l'heure actuelle (par exemple, division par 3)
+int activeSlideIndex = currentTime.getHour() / 3;
+
+model.addAttribute("activeSlideIndex", activeSlideIndex);
+    
             return "fragments/weatherDataFragment";
         } catch (Exception e) {
             return "redirect::/error";
         }
     }
+    
+
 
     // @PostMapping("/index")
     // public String initData(Model model) {
