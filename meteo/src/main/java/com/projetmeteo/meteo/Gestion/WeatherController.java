2c5bc12b08181fa4a48cc087d4d3d1b4c9f801e7
benzar80 <100301600+benzar80@users.noreply.github.com>
2023-12-31 01:32:51 +0100

    Carte Cliquable + Mise à jour du container

--- meteo/src/main/java/com/projetmeteo/meteo/Gestion/WeatherController.java
+++ meteo/src/main/java/com/projetmeteo/meteo/Gestion/WeatherController.java
@@ -87,48 +87,16 @@ public class WeatherController {
     }
 
     @PostMapping("/manageDays")
-    public String initData(Model model) {
+    public String manageDays(Model model) {
         try {
             List<WeatherDataCity> weatherDataList = repo.findAll();
             model.addAttribute("weatherDataLists", weatherDataList);
+            model.addAttribute("city", null);
             return "fragments/manageFragment";
         } catch (Exception e) {
             return "redirect:/error";
         }
     }
-
-    @PostMapping("/takeByAddress")
-    public String takeByAddress(String resolvedAddress, Model model) {
-        try {
-            List<WeatherDataCity> weatherDataList = repo.findAllByResolvedAddress(resolvedAddress);
-            weatherDataList = repo.findAllByResolvedAddress(resolvedAddress);
-            model.addAttribute("weatherDataLists", weatherDataList);
-            // Récupérer la première journée de météo
-            WeatherDataDay firstWeatherDay = weatherDataList.get(0).getWeatherDay().get(0);
-
-            // Récupérer les heures de la première journée
-            List<WeatherDataHour> weatherHours = firstWeatherDay.getWeatherHour();
-
-            // Diviser les heures en groupes de 3
-            List<List<WeatherDataHour>> groupedWeatherHours = Lists.partition(weatherHours, 3);
-                // System.out.println(groupedWeatherHours);
-            // Ajouter la liste groupée au modèle
-            model.addAttribute("groupedWeatherHours", groupedWeatherHours);
-
-                // Obtenez l'heure actuelle
-            LocalTime currentTime = LocalTime.now();
-
-            // Calculez l'index de la slide correspondant à l'heure actuelle (par exemple, division par 3)
-            int activeSlideIndex = currentTime.getHour() / 3;
-
-            model.addAttribute("activeSlideIndex", activeSlideIndex);
-    
-            return "fragments/weatherDataFragment";
-        } catch (Exception e) {
-            return "redirect:/error";
-        }
-    }
-
 
     @PostMapping("/error")
     public String manageError(String city, Model model) {
